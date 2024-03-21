c5a3f11a31561d380f349e15c16f6f25e5961b08
Pavel Ozernov <pavel.ozernov@gmail.com>
2024-03-21 19:01:56 +0500

    HW3 without types in greetings compnt

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -19,20 +19,19 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
-    }
+export const pureAddUserCallback = (name: string, setUsers: any, users: UserType[]) => { // need to fix any
+    const user = { _id: v1(), name}
     setUsers([...users, user])
 }
 
-const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+export const HW3 = () => {
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
@@ -46,7 +45,7 @@ const HW3 = () => {
                     users={users}
                     addUserCallback={addUserCallback}
                 />
-            </div>
+            </div> 
         </div>
     )
 }
